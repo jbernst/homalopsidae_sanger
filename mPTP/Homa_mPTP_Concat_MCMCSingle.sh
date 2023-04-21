8216f994cc1c842ce25dae77ee5749df6abcd08f
Justin Bernstein <jmbernst223@gmail.com>
2023-04-21 18:38:23 -0500

    Create Homa_mPTP_Concat_MCMCSingle.sh

---
+++ mPTP/Homa_mPTP_Concat_MCMCSingle.sh
@@ -0,0 +1,1 @@
+mptp --tree_file Homalopsidae_Cytb-PRLR_4-27-2020.tre --minbr 0.0022448990 --mcmc 500000000 --mcmc_log --mcmc_sample 100000 --single --mcmc_runs 2 --mcmc_burnin 50000000 --output_file Homa_mPTP_Concat_MCMCSingle 
